f0b39c0d076fa1eb7399954edb0d9fc76cb9a3c8
Andrii Denysenko <st4r1r@gmail.com>
2022-10-18 17:48:09 +0300

    Surname has been created

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ def name():
     a = names.get_first_name()
     return a
 
-Pers_name = name()
 
-print('My name is', Pers_name)
+def surname():
+    b = names.get_last_name()
+    return b
+
+
+Pers_name = name()
+Pers_surname = surname()
+print(f'My name is {Pers_name} {Pers_surname}')
